6d09ab463230265cd56eaeb87791e35cb6fcf497
kai.zhang <zhangkai_kai@foxmail.com>
2025-10-09 19:30:03 +0800

    facade: add classes for financial instruments and a test driver

---
+++ chapter12/src/facade1/Stock2.java
@@ -0,0 +1,14 @@
+package facade1;
+
+//股票2
+public class Stock2 {
+    //卖股票
+    public void sell() {
+        System.out.println("股票2卖出");
+    }
+
+    //买股票
+    public void buy() {
+        System.out.println("股票2买入");
+    }
+}
---
+++ chapter12/src/facade1/Test.java
@@ -0,0 +1,35 @@
+package facade1;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("**********************************************");
+        System.out.println("《大话设计模式》代码样例");
+        System.out.println();
+
+        Stock1 stock1 = new Stock1();
+        Stock2 stock2 = new Stock2();
+        NationalDebt1 nd1 = new NationalDebt1();
+        Realty1 rt1 = new Realty1();
+
+        stock1.buy();
+        stock2.buy();
+        nd1.buy();
+        rt1.buy();
+
+        stock1.sell();
+        stock2.sell();
+        nd1.sell();
+        rt1.sell();
+
+
+        System.out.println();
+        System.out.println("**********************************************");
+
+    }
+}
+
+
+
+
